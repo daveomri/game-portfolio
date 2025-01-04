fa0ba00e27f8b1eb763ff9e7ff59a91f2c072342
Dave Omrai <dave@arachne.lan>
2025-01-04 19:54:25 +0100

    Finished project, styles and personal data need to be added

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,15 @@
 export const scaleFactor = 4;
+
+export const dialogueData: {
+    [key: string]: string;
+} = {
+    pc: `todo pc`,
+    tv: `todo tv`,
+    bed: `todo bed`,
+    resume: `todo resume`,
+    projects: `todo projects`,
+    library: `todo library`,
+    'cs-degree': `todo degree`,
+    'sofa-table': `todo sofa`,
+    'exit': `todo exit`
+};
