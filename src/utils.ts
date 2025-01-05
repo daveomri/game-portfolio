65ca02c16e31507c3a5465b9d71e841271727b04
Dave Omrai <dave@arachne.lan>
2025-01-05 13:47:52 +0100

    Added biome, actions, and fixed a code a bit

--- src/utils.ts
+++ src/utils.ts
@@ -1,42 +1,42 @@
 import type { KaboomCtx } from "kaboom";
 
-export function displayDialogue(text: string, onDisplayEnd: ()=>{}) {
-  const dialogueUI = document.getElementById("textbox-container");
-  const dialogue = document.getElementById("dialogue");
+export function displayDialogue(text: string, onDisplayEnd: () => {}) {
+	const dialogueUI = document.getElementById("textbox-container");
+	const dialogue = document.getElementById("dialogue");
 
-  if (dialogueUI) dialogueUI!.style.display = "block";
+	if (dialogueUI) dialogueUI!.style.display = "block";
 
-  let index = 0;
-  let currentText = "";
-  const intervalRef = setInterval(() => {
-    if (index < text.length) {
-      currentText += text[index];
-      dialogue!.innerHTML = currentText;
-      index++;
-      return;
-    }
+	let index = 0;
+	let currentText = "";
+	const intervalRef = setInterval(() => {
+		if (index < text.length) {
+			currentText += text[index];
+			dialogue!.innerHTML = currentText;
+			index++;
+			return;
+		}
 
-    clearInterval(intervalRef);
-  }, 5);
+		clearInterval(intervalRef);
+	}, 5);
 
-  const closeBtn = document.getElementById("close");
+	const closeBtn = document.getElementById("close");
 
-  function onCloseBtnClick() {
-    onDisplayEnd();
-    if (dialogueUI) dialogueUI!.style.display = "none";
-    dialogue!.innerHTML = "";
-    clearInterval(intervalRef);
-    closeBtn?.removeEventListener("click", onCloseBtnClick);
-  }
+	function onCloseBtnClick() {
+		onDisplayEnd();
+		if (dialogueUI) dialogueUI!.style.display = "none";
+		dialogue!.innerHTML = "";
+		clearInterval(intervalRef);
+		closeBtn?.removeEventListener("click", onCloseBtnClick);
+	}
 
-  closeBtn?.addEventListener("click", onCloseBtnClick);
+	closeBtn?.addEventListener("click", onCloseBtnClick);
 }
 
 export function setCamScale(k: KaboomCtx) {
-  const resizeFactor = k.width() / k.height();
-  if (resizeFactor < 1) {
-    k.camScale(k.vec2(1));
-    return;
-  }
-  k.camScale(k.vec2(1.5));
+	const resizeFactor = k.width() / k.height();
+	if (resizeFactor < 1) {
+		k.camScale(k.vec2(1));
+		return;
+	}
+	k.camScale(k.vec2(1.5));
 }
